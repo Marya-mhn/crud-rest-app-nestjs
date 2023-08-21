73e793bfdb7fdfcf35a81e4ae44c66121c985c38
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-08-21 02:31:43 +0100

    crud functions + testing the apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.models';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://machanemarya20:oiHvTavwhBCn2TRE@ac-jynaay9-shard-00-00.vnw8fte.mongodb.net:27017,ac-jynaay9-shard-00-01.vnw8fte.mongodb.net:27017,ac-jynaay9-shard-00-02.vnw8fte.mongodb.net:27017/?replicaSet=atlas-8ugpl9-shard-0&ssl=true&authSource=admin',
     ),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
+import { User, UserDocument } from './user.models';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('user') private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  // create user
+  async createUser(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+
+  // get all users
+  async readUser() {
+    return this.userModel
+      .find({})
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // update user
+  async updateUser(id, data): Promise<User> {
+    return this.userModel.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  // delete user
+  async deleteUser(id) {
+    return this.userModel.findByIdAndDelete(id);
   }
 }
